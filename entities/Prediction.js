5eb1f3950672b1f3be395910da36e84585466e3f
CharlesCousyn <charles.cousyn1@uqac.ca>
2020-01-29 15:25:40 -0500

    Adding possibility to initiate prediction object with JS object

--- entities/Prediction.js
+++ entities/Prediction.js
@@ -2,16 +2,43 @@
 export default class Prediction
 {
     /**
+     * Create results.
+     * @param {string|Object} labelOrObject - Label or object used to initiate a prediction
+     * @param {number=} relevance - The score of relevance
+     * @param {Boolean=} correct - The relevance boolean
+     */
+    constructor(labelOrObject, relevance, correct)
+    {
+        if(typeof labelOrObject === "string")
+        {
+            this._constructWithRawParameters(labelOrObject, relevance, correct)
+        }
+        else if(typeof labelOrObject === "object")
+        {
+            this._constructWithObj(labelOrObject);
+        }
+    }
+
+    /**
      * Create a prediction.
      * @param {string} label - The label
      * @param {number} relevance - The score of relevance
      * @param {Boolean} correct - The relevance boolean
      */
-    constructor(label, relevance, correct)
+    _constructWithRawParameters(label, relevance, correct)
     {
         this.label = label;
         this.relevance = relevance;
         this.correct = correct;
+    }
+
+    /**
+     * Create a prediction.
+     * @param {Object} obj - Object used to initiate prediction
+     */
+    _constructWithObj(obj)
+    {
+        Object.assign(this, obj);
     }
 
     set label(label)
