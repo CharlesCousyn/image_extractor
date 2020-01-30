b2527beaa4e7fd2adfdca36af802506c490c1337
CharlesCousyn <charles.cousyn1@uqac.ca>
2020-01-30 18:48:22 -0500

    Writing evaluation function for each combination(first try)

--- experimentation.js
+++ experimentation.js
@@ -1,6 +1,10 @@
 import EXPERIMENTATIONS_CONFIG from './configFiles/experimentationsConfig.json'
 import RUN from "./main.js"
 import mAP from "./performanceMetrics.js"
+import filesSystem from "fs";
+import groundTruth from "./configFiles/groundTruth.json"
+import Prediction from "./entities/Prediction";
+import Results from "./entities/Results";
 
 function generateCombination(criteria)
 {
@@ -43,13 +47,46 @@ function generateCombination(criteria)
         await RUN(...combinations[i]);
 
         //Evaluate on combination
-
-
+        evaluateComb(combinations[i], groundTruth, 25);
     }
 
 })();
 
-function evaluateComb(combination)
+function evaluateComb(combination, groundTruth, k)
 {
+    const path = `resultFiles/${combination.join(" ")}`;
+    const filePaths = filesSystem.readdirSync( path, { encoding: 'utf8', withFileTypes: true })
+        .filter(dirent => dirent.isFile())
+        .map(dirent => `${path}/${dirent.name}`);
 
+    const resultObjects = filePaths.map(filePath =>
+    {
+        //Get resultFile
+        const resultFile = JSON.parse(filesSystem.readFileSync(filePath));
+
+        //Get corresponding groundTruthPreds (with the use of activity name)
+        const groundTruthPreds = groundTruth.find(elem => elem.query === resultFile.query).data.map( item => new Prediction(item));
+
+        //Get corresponding realPreds
+        const realPreds = JSON.parse(filesSystem.readFileSync(filePath)).data.map(item => new Prediction(item));
+
+        //Construct our result object
+        return new Results(resultFile.query, realPreds, groundTruthPreds);
+    });
+
+    //Calculate mAP
+    const meanAveragePrecision = mAP(k, "trapezoidal", resultObjects);
+
+
+    //Get all queries
+    const queries = resultObjects.map(res => res.query);
+
+    //Write file of combination
+    const jsonFinalResult = {queries, meanAveragePrecision};
+    writeJSONFile(jsonFinalResult, `${path}/finalResult.json`);
+}
+
+function writeJSONFile(data, path)
+{
+    filesSystem.writeFileSync(path, JSON.stringify(data, null, 4), "utf8");
 }
